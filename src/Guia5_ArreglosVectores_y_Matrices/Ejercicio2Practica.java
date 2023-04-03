0d054e2726d802dbd22c98a0b59d31ac72ca71bc
Napp1996 <Nicoprado1996@gmail.com>
2023-04-03 19:56:18 -0300

    Segundo comit prueba

--- src/Guia5_ArreglosVectores_y_Matrices/Ejercicio2Practica.java
+++ src/Guia5_ArreglosVectores_y_Matrices/Ejercicio2Practica.java
@@ -18,43 +18,53 @@ public class Ejercicio2Practica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        // Crear objetos para leer de la entrada estándar y generar números aleatorios
         Scanner leer = new Scanner(System.in);
         Random numAleatorio = new Random();
         
+        // Pedir al usuario el tamaño del vector
         System.out.println("Ingrese el tamaño del vector");
         int n = leer.nextInt();
         
+        // Crear un vector de tamaño n y llenarlo con valores aleatorios
         int[] num = new int[n];
         for (int i = 0; i < num.length; i++){
             num[i] = numAleatorio.nextInt(n);           
         }
             
-        
+        // Pedir al usuario el número a buscar
         System.out.println("Ingrese el numero a buscar");
         int numBuscado = leer.nextInt();
+        
+        // Inicializar variables para almacenar la posición donde se encontró el número buscado y la cantidad de repeticiones
         int numPosicion = -1;
         int numRepeticiones = 0;
         
-          System.out.println("El vector es: ");
+        // Imprimir el vector
+        System.out.println("El vector es: ");
         for (int i = 0; i < num.length; i++){
             System.out.println(num[i] + " ");
         }
         System.out.println();
         
-        
+        // Recorrer el vector para buscar el número buscado y contar sus repeticiones
         for (int i = 0; i < num.length; i++){
-            if (num[i] == numBuscado){
-                if (numPosicion == -1){
-                    numPosicion = i;
+            if (num[i] == numBuscado){ // Si el número actual es igual al número buscado
+                if (numPosicion == -1){ // Si es la primera vez que se encuentra el número buscado
+                    numPosicion = i; // Almacenar la posición donde se encontró el número buscado
                 }
-                numRepeticiones++;
+                numRepeticiones++; // Incrementar la cantidad de repeticiones
             }
         }
         
+        /* Mostrar la posición donde se encontró el número buscado (si se encontró) 
+           y la cantidad de repeticiones (si el número está repetido)
+        */
         if (numPosicion != -1){
            System.out.println("El numero " + numBuscado + " se encuentra en la posicion " + numPosicion);
            if (numRepeticiones > 1){
-               System.out.println("el numero " + numBuscado + " esta repetido" + (numRepeticiones - 1) + " veces");
+               System.out.println("el numero " + numBuscado + " esta repetido " + (numRepeticiones - 1) + " veces");
            }
         } else {
             System.out.println("El numero " + numBuscado + " no se ha encontrado en el vector" );    
---
+++ src/guia6/Git_Github_parte2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia6;
+
+/**
+ *
+ * @author nicop
+ */
+public class Git_Github_parte2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
